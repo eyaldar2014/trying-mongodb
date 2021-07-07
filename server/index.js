7b506e8ee65160a75d7e489d7e92ddb3a1105387
Eyal Dar <eyaldar2014@gmail.com>
2021-07-07 10:50:21 +0300

    deploying 1

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
-require('./server/db/mongoose')
+require('./db/mongoose')
 const express = require('express')
-const usersRouter = require('./server/routes/route.user')
-const tasksRouter = require('./server/routes/route.task')
+const usersRouter = require('./routes/route.user')
+const tasksRouter = require('./routes/route.task')
 // const maintenanceMiddleware = require('./server/middlewares/middleware.maintenance')
 
 
 const app = express()
-// const port = process.env.Port || 5000
+// const port = process.env.PORT || 5000
 // MODIFIED to 'enviroment veribal' :
-
-const port = process.env.Port
+const port = process.env.PORT
 
 
 // makes the request property into json :
@@ -31,6 +30,6 @@ app.use('/tasks', tasksRouter)
 
 
 // used to bind the connections on the specified port and activate it :
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('listnening on port', port)
 })
